1be310ea18e45055ba26c6aba5236ffea8b43b2f
Andrew <re.admit.re@gmail.com>
2013-07-10 18:42:16 +0400

    First deploy

--- js/views/filtersView.js
+++ js/views/filtersView.js
@@ -4,15 +4,14 @@ define(['jquery',
         'underscore',
         'backbone',
         'text!templates/selectorTemplate.html',
-//        'jqueryui'
-],
+        'jqueryui'],
   function($, _, Backbone, selectorTemplate) {
     var Selector = Backbone.View.extend({
       el: $('#filters'),
       template: _.template(selectorTemplate),
       render: function() {
         this.$el.html(this.template())
-        //$('#slider').slider()
+        $('#slider').slider()
       }
     })
 
