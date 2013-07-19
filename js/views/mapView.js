ecd6160d44727ee21452c493b992162853e9c6e2
Andrew <re.admit.re@gmail.com>
2013-07-19 21:40:11 +0400

    Base searchbox logig

--- js/views/mapView.js
+++ js/views/mapView.js
@@ -3,20 +3,30 @@
 define(['jquery',
         'underscore',
         'backbone',
+        'models/search',
         'lib/leafletMapProvider',
         'text!templates/searchTemplate.html'],
-  function($, _, Backbone, leafletMapProvider, searchTemplate){
+  function($, _, Backbone, SearchModel, leafletMapProvider, searchTemplate){
     var SearchBox = Backbone.View.extend({
-      el: $('#map-wrapper'),
+      el: $('#searchbox'),
       template: _.template(searchTemplate),
+
+      events: { "click button": "doSearch" },
+      doSearch: function( event ) {
+        var search = new SearchModel.SearchBox({
+          query: this.$("input[name='search']").val()
+        })
+      },
+
       render: function() {
         this.$el.html(this.template())
       }
     })
 
     var Map = Backbone.View.extend({
+      el: $('#map'),
       render: function(lat, lon, zoom) {
-        var mainLayer = new leafletMapProvider.MainLayer('map', lat, lon, zoom)
+        var mainLayer = new leafletMapProvider.MainLayer(this.el, lat, lon, zoom)
       }
     })
 
